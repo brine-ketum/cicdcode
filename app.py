3fb233744e1158e91d4983c5ebbc898bb8ae2311
brine-ketum <168510402+brine-ketum@users.noreply.github.com>
2024-04-30 18:25:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+    return 'Yeyyyy, i have successfully build a fully automated CICD pipeline!!!'
